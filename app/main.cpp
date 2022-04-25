e0014b4f1b32c2bc257ecbe29627f349d8ce8a92
Sergii Liebodkin <s.lebedkin@gmail.com>
2022-04-25 23:08:43 +0300

    update sample orders id

--- app/main.cpp
+++ app/main.cpp
@@ -2,10 +2,10 @@
 #include "DSOrderTypes.hpp"
 
 int main(int argc, char** argv) {
-    auto order = DSOrder::create();
+    auto order = DSOrder::create("OrderId_01");
     order->ModelElements = {
-        DSModelElement::create("ME-01"),
-        DSModelElement::create("ME-02")
+        DSModelElement::create("ModelElementId_01"),
+        DSModelElement::create("ModelElementId_02")
     };
     std::cout << "Hello, World!" << std::endl;
     return 0;

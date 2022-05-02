6e3571967a5f6e80e62c5bffdbab9103c5908d55
Sergii Liebodkin <s.lebedkin@gmail.com>
2022-05-02 18:11:39 +0300

    fix compilation error

--- app/main.cpp
+++ app/main.cpp
@@ -3,9 +3,9 @@
 
 int main(int argc, char** argv) {
     auto order = DSOrder::create("OrderId_01");
-    order->ModelElements = { DSModelElement::create("ModelElementId_01"),
+    order->modelElements = { DSModelElement::create("ModelElementId_01"),
                              DSModelElement::create("ModelElementId_02") };
-    for (const auto& me: order->ModelElements)
+    for (const auto& me: order->modelElements)
         std::cout << me->id << std::endl;
     return 0;
 }
